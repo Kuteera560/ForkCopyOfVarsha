7b5f7b57ee8f00b245ebcc5780543336107885ad
Pari560 <168195705+Pari560@users.noreply.github.com>
2024-10-18 01:45:13 +0530

    AteParimala updated test/commit msg

---
+++ Architect/CRMArchitect_gitHub/src/test/java/Parimala/ParimalaATEmodeTest.java
@@ -0,0 +1,11 @@
+package Parimala;
+
+import org.testng.annotations.Test;
+
+public class ParimalaATEmodeTest {
+	@Test
+	public void print(){
+		System.out.println("Message:Updated from ATE Pari");
+	}
+
+}
